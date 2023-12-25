5f3d964ff538bc9bc713651be2106341e865076a
davi-nari <129951508+davi-nari@users.noreply.github.com>
2023-12-25 23:14:35 +0500

    Add files via upload

---
+++ assets/Movie-d8b28694.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Media-15d71b12.js";import{o,c as r}from"./index-13cc2c11.js";const m={__name:"Movie",setup(t){return(_,a)=>(o(),r(e,{type:"movie"}))}};export{m as default};
---
+++ assets/MoviesPage-89c9ca98.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./Content-f102a2b4.js";import{o,c as a}from"./index-13cc2c11.js";const m={__name:"MoviesPage",setup(r){return(t,_)=>(o(),a(e,{type:"movie"}))}};export{m as default};
